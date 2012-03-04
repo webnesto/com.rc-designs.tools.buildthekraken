8b9ebe6bc5390df7b3375d19f3898f46b0bd2051
ernesto johnson <nesto@rc-designs.com>
2012-03-04 13:04:56 -0800

    * updated example files

--- exampleSrc/tmp/min/test.js
+++ exampleSrc/tmp/min/test.js
@@ -1,2 +1,2 @@
 /*! GENERATED FILE - DO NOT EDIT */
-document.write('<scr'+'ipt type="text/javascript" src="tmp/test.js"></scr'+'ipt>')
+document.write('<scr'+'ipt type="text/javascript" src="../exampleSrc/test/cow.js"></scr'+'ipt>')
